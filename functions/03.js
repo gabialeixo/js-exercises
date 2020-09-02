b41fbad1795aac84d69db59505dc6ab90a76d394
Gabriele Aleixo <gabealeixo13@gmail.com>
2020-09-02 19:24:42 -0300

    add exercicio 03

---
+++ functions/03.js
@@ -0,0 +1,7 @@
+/* 03) Crie uma função que recebe dois parâmetros, base e expoente, e retorne a base elevada ao expoente */
+
+const expo = (b, e) => {
+    return Math.pow(b, e)
+}
+
+console.log(expo(5, 7))
