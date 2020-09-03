d207efe034d7bf40cb959c80339e6f88ee52ebd0
Gabriele Aleixo <gabealeixo13@gmail.com>
2020-09-03 15:53:13 -0300

    add exercicio 07

---
+++ functions/07.js
@@ -0,0 +1,27 @@
+/* 07) Uma das vantagens da programação é a automatização de tarefas que não gostamos de realizar. Dito isto,
+elabore uma função cujo objetivo é resolver a fórmula de Bhaskara. Para isso, sua função deve receber três
+parâmetros, “ax2”, “bx” e “c”, de tal modo que na equação: 3x² - 5x + 12 os valores seriam respectivamente: 3,
+-5, 12. Como retorno deve ser passado um vetor que tem 2 valores um para cada possível resultado, mesmo
+que os resultados sejam iguais. Caso o delta seja negativo, retorne, ao invés do vetor, um string com a frase:
+“Delta é negativo”. */
+
+const baskhara = (ax2, bx, c) => {
+    let resultado = []
+    let delta = (bx ** 2) - (4 * ax2 * c)
+    if(delta < 0){
+        return 'Delta é negativo!'
+    }
+
+    let x1 = (-bx + Math.sqrt(delta)) / 2 * ax2
+    let x2 = (-bx - Math.sqrt(delta)) / 2 * ax2
+
+    resultado.push(x1)
+    resultado.push(x2)
+    return resultado
+}
+
+console.log(baskhara(3, -5, 12))
+console.log(baskhara(1, 2, 3))
+console.log(baskhara(3, 2, 1))
+console.log(baskhara(3, 3, 3))
+console.log(baskhara(1, 8, -9))
